67f4f4ac7c57d4179ce27f2fed169585a5b3a80c
itsoluwatobby <oluwasamuel050@gmail.com>
2024-03-14 21:00:56 +0100

    presently on task 6

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+import math
+"""
+a type-annotated function floor which takes a float n as argument
+and returns the floor of the float.
+"""
+
+
+def floor(n: float) -> float:
+    """return the floor of a float"""
+    return math.floor(n)
---
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+a type-annotated function sum_list which takes a list input_list of
+floats as argument and returns their sum as a float.
+"""
+
+
+def sum_list(input_list: list[float]) -> float:
+    """Returns a sum of floats"""
+    sum_float = 0
+    for num in input_list:
+        sum_float += num
+    return sum_float
