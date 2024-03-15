9579d88a1aa9d91bed5a5ff34a19f72be23e3a36
itsoluwatobby <oluwasamuel050@gmail.com>
2024-03-15 03:22:03 +0100

    chore(optional-tasks): working on optional tasks

--- 0x00-python_variable_annotations/2-floor.py
+++ 0x00-python_variable_annotations/2-floor.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python3
-import math
 """
 a type-annotated function floor which takes a float n as argument
 and returns the floor of the float.
@@ -11,4 +10,4 @@ def floor(n: float) -> float:
     A function that takes in a float n as argument and
     turn the floor of a float
     """
-    return math.floor(n)
+    return int(n) if n >= 0 else int(n) - 1
