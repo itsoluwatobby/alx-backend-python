7f681e7676e425b3b7ba5a90fad7152c9c49a31a
Oluwatobi Akinola Samuel <oluwasamuel050@gmail.com>
2024-03-19 18:53:31 +0100

    Update 0-async_generator.py

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -8,9 +8,10 @@ random module.
 """
 import asyncio
 import random
+from typing import Generator
 
 
-async def async_generator() -> None:
+async def async_generator() -> Generator[float, None, None]:
     """A coroutine that loops 10 times"""
     for _ in range(10):
         await asyncio.sleep(1)
